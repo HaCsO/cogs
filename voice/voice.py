f39ada708d2a1495b1393d4ba29d219c912b651f
HaCsO <48949815+HaCsO@users.noreply.github.com>
2020-02-13 09:13:02 +0300

    Update voice.py

--- voice/voice.py
+++ voice/voice.py
@@ -21,16 +21,7 @@ class Voice(commands.Cog):
 				id = int(cfg['ROLES'][f'role{i}'])
 				prise = int(cfg['PRISE'][f'role{i}'])
 				self.roles[f'role{i}'] = {'id': f'{id}', 'prise': prise}
-				
-	@commands.Cog.listener()
-	async def on_message(self, msg):
-		pass
 
-	@commands.Cog.listener()
-	async def on_member_join(self, member):
-		role = discord.utils.get(member.guild.roles, id=487691377346347009)
-		await member.add_roles(role)
-				
 	@commands.Cog.listener()
 	async def on_voice_state_update(self, member, before, after):
 		if member.bot:
